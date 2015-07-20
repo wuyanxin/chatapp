7be64269ce83a7df0f405883c2cd7d5d8c6b895c
wuyanxin <elsonwoolf@gmail.com>
2015-07-20 21:12:27 +0800

    no message

--- api/services/SocketService.js
+++ api/services/SocketService.js
@@ -1,8 +1,33 @@
+var IO = require('socket.io');
+var socketIO = null;
+
 /**
-  * Copyright (c) 2015 Meizu bigertech, All rights reserved.
-  * http://www.bigertech.com/
-  * @author wuyanxin
-  * @date  15/7/20
-  * @description 
-  *  
-  */
+ * 创建socket服务器
+ * @param server
+ * @returns {*}
+ */
+function create(server) {
+  var socketIO = IO(server);
+
+  socketIO.on('connection', function (socket) {
+    console.log('a user connected');
+  });
+
+  return socketIO;
+}
+
+/**
+ * 获取socket服务器对象
+ * @returns {*}
+ */
+function getIO() {
+  if (!socketIO) {
+    throw new Error('socket.io is not inited');
+  }
+  return socketIO;
+}
+
+module.exports = {
+  create: create,
+  getIO: getIO
+};
--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var session = require('express-session');
 var path = require('path');
+var createSocket = require('./api/services/SocketService').create;
 var app = express();
+var server = require('http').Server(app);
 
 app.use(session({
   secret: 'JHD=JF@%$WHATEVER',
@@ -10,5 +12,11 @@ app.use(session({
   cookie: { secure: true }
 }));
 app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
 
-app.listen(9826);
+createSocket(server);
+
+server.listen(9826, function () {
+  console.log('server listening port 9826');
+});
