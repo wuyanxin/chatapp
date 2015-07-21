2b9f097aa4a1c30eb4f13798923127887a41a8b3
wuyanxin <yanxin.wu@qq.com>
2015-07-21 22:06:03 +0800

    Add RoomController

---
+++ api/controllers/RoomController.js
@@ -0,0 +1,63 @@
+/**
+  * Copyright (c) 2015 Meizu bigertech, All rights reserved.
+  * http://www.bigertech.com/
+  * @author wuyanxin
+  * @date  15/7/21
+  * @description 
+  *  
+  */
+
+var RoomService = require('../services/RoomService');
+
+/**
+ * 创建房间
+ * @param req
+ * @param res
+ */
+function create(req, res) {
+  var roomName = req.params.roomName;
+  var userSID = req.sessionID;
+  var username = req.session.nickname;
+
+  var room = RoomService.createRoom(roomName, userSID, username);
+  //res.render('/room', {room: room});
+  res.send({room: room});
+}
+
+/**
+ * 加入房间
+ * @param req
+ * @param res
+ */
+function join(req, res) {
+  var roomID = req.params.roomID;
+  var userSID = req.sessionID;
+  var username = req.session.nickname;
+
+  try {
+    var room = RoomService.joinRoom(roomID, userSID, username);
+    //res.render('/room', {room: room});
+    res.send({room: room});
+  } catch (e) {
+    res.send(e.message);
+  }
+}
+
+/**
+ * 离开房间
+ * @param req
+ * @param res
+ */
+function leave(req, res) {
+  var roomID = req.params.roomID;
+  var userSID = req.sessionID;
+
+  var result = RoomService.leaveRoom(roomID, userSID);
+  res.send({result: result});
+}
+
+module.exports = {
+  create: create,
+  join: join,
+  leave: leave
+};
--- config/router.js
+++ config/router.js
@@ -2,13 +2,19 @@ var express = require('express');
 var router = express.Router();
 var UserController = require('../api/controllers/UserController');
 
+// index
 router.get('/', function (req, res) {
-  console.log(req.session.nickname);
   res.render('index', {
     nickname: req.session.nickname
   });
 });
 
+// user
 router.get('/user/setName/:name', UserController.setName);
+
+// room
+router.get('/room/create/:roomName', RoomController.create);
+router.get('/room/join/:roomID', RoomController.join);
+router.get('/room/leave/:roomID', RoomController.leave);
 
 module.exports = router;
