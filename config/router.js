4a79c8fe3da6d92cf785571f8b4481c0eeaf0f9a
wuyanxin <yanxin.wu@qq.com>
2015-07-21 22:09:00 +0800

    fix router error

--- config/router.js
+++ config/router.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var UserController = require('../api/controllers/UserController');
+var RoomController = require('../api/controllers/RoomController');
 
 // index
 router.get('/', function (req, res) {
