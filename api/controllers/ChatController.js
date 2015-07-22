f54a77afe22ac0b8c6b50199ec370a368e9c24a6
wuyanxin <yanxin.wu@qq.com>
2015-07-22 09:42:22 +0800

    add ChatController

---
+++ api/controllers/ChatController.js
@@ -0,0 +1,16 @@
+/**
+  * Copyright (c) 2015 Meizu bigertech, All rights reserved.
+  * http://www.bigertech.com/
+  * @author wuyanxin
+  * @date  15/7/21
+  * @description 
+  *  
+  */
+
+var SocketService = require('../services/SocketService');
+
+function say(req, res) {
+  var roomID = req.params.roomID;
+  var userSID = req.sessionID;
+
+}
